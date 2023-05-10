3b495d729ab21bc5c592066e150dfcffa1a341c8
akane-aoki <okaka3080annietomorrow@gmail.com>
2023-05-10 12:13:14 +0900

    Add: form入力、確認、完了画面遷移

--- form/input.php
+++ form/input.php
@@ -1,10 +1,19 @@
 <?php
 
 if(!empty($_POST)){
-//スーパーグローバル変数
   echo '<pre>';
   var_dump($_POST);
   echo '</pre>';
+}
+
+// 入力、確認、完了を切り替える
+$pageFlag = 0;
+
+if(!empty($_POST['btn_confirm'])){
+  $pageFlag = 1;
+}
+if(!empty($_POST['btn_submit'])){
+  $pageFlag = 2;
 }
 
 ?>
@@ -14,17 +23,37 @@ if(!empty($_POST)){
 <head></head>
 
 <body>
+
+<?php if($pageFlag === 1) : ?>
+<form method="POST" action="input.php">
+氏名
+<?php echo $_POST['your_name'] ;?>
+<br>
+メールアドレス
+<?php echo $_POST['email'] ;?>
+<br>
+<input type="submit" name="btn_submit" value="送信する">
+<!-- hiddenにすると、表示はされないが、裏で値を持っておくことができる -->
+<input type="hidden" name="your_name" value="<?php echo $_POST['your_name'] ;?>">
+<input type="hidden" name="email" value="<?php echo $_POST['email'] ;?>">
+</form>
+<?php endif; ?>
+
+<?php if($pageFlag === 2) : ?>
+送信が完了しました。
+<?php endif; ?>
+
+<?php if($pageFlag === 0) : ?>
 <form method="post" action="input.php">
 氏名
 <input type="text" name="your_name">
 <br>
-<input type="checkbox" name="sports[]" value="野球">野球
-<input type="checkbox" name="sports[]" value="サッカー">サッカー
-<input type="checkbox" name="sports[]" value="バスケ">バスケ
-
-<input type="submit" value="送信">
-
+メールアドレス
+<input type="email" name="email">
+<br>
+<input type="submit" name="btn_confirm" value="確認する">
 </form>
+<?php endif; ?>
 
 </body>
 </html>
