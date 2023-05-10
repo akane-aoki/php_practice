7bcaa6efd28fe0c004ae68ac699725345ba68fe7
akane-aoki <okaka3080annietomorrow@gmail.com>
2023-05-10 13:48:16 +0900

    Add: formの確認画面に戻るボタン作成

--- form/input.php
+++ form/input.php
@@ -24,6 +24,7 @@ if(!empty($_POST['btn_submit'])){
 
 <body>
 
+<!-- 確認画面 -->
 <?php if($pageFlag === 1) : ?>
 <form method="POST" action="input.php">
 氏名
@@ -32,6 +33,7 @@ if(!empty($_POST['btn_submit'])){
 メールアドレス
 <?php echo $_POST['email'] ;?>
 <br>
+<input type="submit" name="back" value="戻る">
 <input type="submit" name="btn_submit" value="送信する">
 <!-- hiddenにすると、表示はされないが、裏で値を持っておくことができる -->
 <input type="hidden" name="your_name" value="<?php echo $_POST['your_name'] ;?>">
@@ -39,17 +41,19 @@ if(!empty($_POST['btn_submit'])){
 </form>
 <?php endif; ?>
 
+<!-- 完了画面 -->
 <?php if($pageFlag === 2) : ?>
 送信が完了しました。
 <?php endif; ?>
 
+<!-- 入力画面 -->
 <?php if($pageFlag === 0) : ?>
 <form method="post" action="input.php">
 氏名
-<input type="text" name="your_name">
+<input type="text" name="your_name" value="<?php if(!empty($_POST['your_name'])){echo $_POST['your_name'] ;} ?>">
 <br>
 メールアドレス
-<input type="email" name="email">
+<input type="email" name="email" value="<?php if(!empty($_POST['email'])){echo $_POST['email'] ;} ?>">
 <br>
 <input type="submit" name="btn_confirm" value="確認する">
 </form>
